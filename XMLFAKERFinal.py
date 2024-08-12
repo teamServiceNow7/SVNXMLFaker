07745f525eec1079a3b120ac437ce8c275d3dc0f
teamServiceNow7 <teamservicenow7@gmail.com>
2024-08-12 15:59:11 +0800

    Update XMLFAKERFinal.py

--- XMLFAKERFinal.py
+++ XMLFAKERFinal.py
@@ -405,7 +405,9 @@ def save_modified_xml(file_name, tree):
 #Main Function 
 def main():
     st.image("XML_TitleHeader.png")
-    
+    #st.title("ServiceNow ENGINEERING DEMO DATA MODIFIER")
+    #st.divider()
+
     progress_text = "Operation in progress. Please wait."
     my_bar = st.progress(0, text=progress_text)
 
@@ -429,13 +431,16 @@ def main():
             if uploaded_file.name == selected_file_name:
                 selected_file = uploaded_file
                 break
-
+ 
         if selected_file:
+           
             file_name = selected_file.name
+
+            # Remove the prefix, file extension, and underscores, then convert to proper case
             display_file_name = file_name.replace("samp_eng_app_", "").replace("_", " ").rsplit('.', 1)[0].title()
 
             st.header(f"Update {display_file_name}")
-
+           
             # Load and parse the XML file
             tree = ET.parse(selected_file)
             root = tree.getroot()
@@ -444,77 +449,86 @@ def main():
             concurrent = root.find('.//samp_eng_app_concurrent_usage[@action="INSERT_OR_UPDATE"]')
             denial = root.find('.//samp_eng_app_denial[@action="INSERT_OR_UPDATE"]')
          
+            # Find all <samp_eng_app_concurrent_usage> elements with the specified action attribute
             if usage:
                 usage_elements = root.findall('.//samp_eng_app_usage_summary[@action="INSERT_OR_UPDATE"]')
+               
             elif concurrent:
                 usage_elements = root.findall('.//samp_eng_app_concurrent_usage[@action="INSERT_OR_UPDATE"]')
+    
             elif denial:
                 usage_elements = root.findall('.//samp_eng_app_denial[@action="INSERT_OR_UPDATE"]')
-                
+                # Count the elements
             count = len(usage_elements)
 
-            min_range, max_range = st.sidebar.slider("Select Range", min_value=1, max_value=count, value=(1, count), key="select_range")
+            min_range, max_range = st.sidebar.slider("Select Range",min_value=1, max_value=count,value=(1,count),key="select_range")
+            # Fields that are always visible
             new_source = st.sidebar.text_input("New Source Value", "")
+           
             st.sidebar.subheader("New Date Value", "")
 
+            # Determine the appropriate label [EDITED  ]
             if denial:
                 label = "Update Denial Date"
             else:
                 label = "Update Usage Date"
 
+            # Display the date input with the corresponding label
             with st.sidebar.expander(f"#### {label}"):
                 st.markdown("")
-                new_date = st.date_input("Enter Start Date", value=None)
+                new_date = st.date_input("Enter Start Date",value=None)
 
             if usage:
-                with st.sidebar.expander("#### Update Idle Duration"):
+                with st.sidebar.expander(f"#### {"Update Idle Duration"}"):
                     st.markdown("")
-                    idle_dur_date = st.date_input("Enter Idle Duration (Date)", value=None)
-                    idle_dur_time = st.time_input("Enter Idle Duration (Time)", value=None, step=60)
+                    idle_dur_date = st.date_input("Enter Idle Duration (Date)",value=None)
+                    idle_dur_time = st.time_input("Enter Idle Duration (Time)",value=None,step=60)
                     
-                with st.sidebar.expander("#### Session Duration"):
+                with st.sidebar.expander(f"#### {"Session Duration"}"):
                     st.markdown("")
-                    session_dur_date = st.date_input("Enter Session Duration (Date)", value=None)
-                    session_dur_time = st.time_input("Enter Session Duration (Time)", value=None, step=60)
+                    session_dur_date = st.date_input("Enter Session Duration (Date)",value=None)
+                    session_dur_time = st.time_input("Enter Session Duration (Time)",value=None,step=60)
 
-                if idle_dur_date and idle_dur_time:
-                    total_idle_dur = datetime.combine(idle_dur_date, idle_dur_time)
+                if((idle_dur_date is not None) and (idle_dur_time is not None)):
+                    total_idle_dur = datetime.combine(idle_dur_date,idle_dur_time)
                 else:
                     total_idle_dur = None            
-                if session_dur_date and session_dur_time:        
-                    total_session_dur = datetime.combine(session_dur_date, session_dur_time)
+                if((session_dur_date is not None) and (session_dur_time is not None) ):        
+                    total_session_dur = datetime.combine(session_dur_date,session_dur_time)
                 else:
                     total_session_dur = None
     
+            
             update_button = st.sidebar.button("Update All Fields")
             st.sidebar.divider()
 
+ 
             if usage:
-                tree = parse_usage_summary(tree, root, min_range, max_range, new_source if update_button else None, new_date if update_button else None, total_idle_dur if update_button else None, total_session_dur if update_button else None)
+                tree = parse_usage_summary(tree,root,min_range,max_range, new_source if update_button else None, new_date if update_button else None,total_idle_dur if update_button else None, total_session_dur if update_button else None)
+
             elif concurrent:
-                tree = parse_concurrent_usage(tree, root, min_range, max_range, new_source if update_button else None, new_date if update_button else None)
+                tree = parse_concurrent_usage(tree,root,min_range,max_range, new_source if update_button else None, new_date if update_button else None)
+           
             elif denial:
-                tree = parse_denial(tree, root, min_range, max_range, new_source if update_button else None, new_date if update_button else None)
+                tree = parse_denial(tree,root,min_range,max_range, new_source if update_button else None, new_date if update_button else None)
+                
             else:
                 st.write(f"Unknown file type: {file_name}")
                 return
             
             if update_button:
                 modified_xml = save_modified_xml(file_name, tree)
+                #selected_file = modified_xml
                 st.sidebar.download_button(
-                    label="Download Modified XML",
-                    data=modified_xml,
-                    file_name=file_name,
-                    mime='application/xml'
+                label="Download Modified XML",
+                data = modified_xml,    
+                file_name=file_name,
+                mime='application/xml',
+                type="primary"
                 )
                 st.sidebar.divider()
                 st.success(":white_check_mark: All fields updated successfully!")
-
-                # Simulating updating the file in the uploader
-                # We need to re-upload the modified file, which is not directly possible.
-                # However, we can simulate this by showing the updated content and prompting user action.
-                st.write("Updated file content:")
-                st.text_area("Updated XML Content", value=modified_xml.decode('utf-8'), height=300) 
+ 
 
 
 
